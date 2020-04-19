0b4fa2ecf16c6e4ba270a00ec8661aa51de4e24a
sherrera0198 <sherrera0198@gmail.com>
2020-04-19 02:45:31 -0400

    Avance en preprocesamiento-seba , y doc - Sebastian Herrera

--- pre-procesamienento - Seba.py
+++ pre-procesamienento - Seba.py
@@ -1,11 +1,20 @@
 import pandas as pd
-from pandas import *
 from sklearn.preprocessing import Imputer
 from sklearn.impute import SimpleImputer
 import os
 
 
-df_retail = pandas.read_excel('codigo/BD/OnlineRetail.xlsx')
-df_retail.head()
+print  ("inicio")
+df_retail = pd.read_excel('codigo/BD/OnlineRetail.xlsx')
+print (df_retail.info())
+headers  = df_retail.columns
 
 
+for  column in headers:
+    if df_retail[column].dtype  in ("int64", float) :
+        print( "la columna {} tiene un promedio de =  {} " .format(column,  df_retail[column].mean()))
+        print( "la columna {} tiene un mediana de =  {} " .format(column,  df_retail[column].median()))
+        print( "la columna {} tiene una desviación estandar  de =  {} " .format(column,  df_retail[column].std()))
+        print( "la columna {} tiene un máximo de =  {} " .format(column,  df_retail[column].max()))
+        print( "la columna {} tiene un mínimo de =  {} " .format(column,  df_retail[column].min()))
+        print ("-----------------------------------------------------------------------------")
