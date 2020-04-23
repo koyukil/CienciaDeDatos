b24dfe82faf86f374895cf24291c6cfe3f0bc088
sherrera0198 <sherrera0198@gmail.com>
2020-04-22 22:14:15 -0400

    datos 1d - Sebastián Herrera

--- pre-procesamienento - Seba.py
+++ pre-procesamienento - Seba.py
@@ -6,6 +6,7 @@ import os
 
 print  ("inicio")
 df_retail = pd.read_excel('codigo/BD/OnlineRetail.xlsx')
+df_retail.iloc[:, 0] = df_retail.rename({"voiceNo" :"Numero_compra", "StockCode": "Codigo_producto","Description": "Descripcion", "Quantity": "Cantidad", "InvoiceDate":"Fecha_compra","UnitPrice":"Precio_unitario","CustomerID":"Id_cliente","Country": "Pais"}, axis ='columns')
 print (df_retail.info())
 headers  = df_retail.columns
 
@@ -17,4 +18,7 @@ for  column in headers:
         print( "la columna {} tiene una desviación estandar  de =  {} " .format(column,  df_retail[column].std()))
         print( "la columna {} tiene un máximo de =  {} " .format(column,  df_retail[column].max()))
         print( "la columna {} tiene un mínimo de =  {} " .format(column,  df_retail[column].min()))
+        print ("-----------------------------------------------------------------------------")
+    if  df_retail[column].dtype  in ("object", "datetime64[ns]" ) :
+        print( "la columna {} tiene como moda a =  {} " .format(column,  df_retail[column].mode()))
         print ("-----------------------------------------------------------------------------")
