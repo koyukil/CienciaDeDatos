9e4def5fb3dc4c0e20770be98cb6d21a8e88ebfb
sherrera0198 <49920903+sherrera0198@users.noreply.github.com>
2020-05-29 21:10:42 -0400

    modularización de funciones fase 2 - Sebastián Herrera

--- codigo/fase2.py
+++ codigo/fase2.py
@@ -1,11 +1,4 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.impute import SimpleImputer
-import seaborn as sns
-import os
-import subprocess
-
+from fase2_funciones import *
 
 df_bd_preprocesada = pd.read_csv('BD/OnlineRetail_Preprocesada.csv', sep=',', encoding = 'unicode_escape')
 df_pivot = df_bd_preprocesada['InvoiceDate'].str.split(expand = True)
@@ -16,47 +9,7 @@ df_bd_preprocesada['Monto'] = df_bd_preprocesada['UnitPrice'] * df_bd_preprocesa
 
 
 
-#-------------------------cuantos articulos en promedio se compra en cada transacción---------------------
-df_pivot = df_bd_preprocesada[['InvoiceNo','Quantity', 'Monto']].groupby(['InvoiceNo']).sum()
-df_pivot=df_pivot.reset_index()
-print('El promedio de articulos por compra es  {} ' .format(df_pivot['Quantity'].mean()))
-print('la desviación estandar en cantidad de articulos por compra es  {} ' .format(df_pivot['Quantity'].std()))
-#-----El resultado es muy alto y poco real, eliminar valores demasiado altos para  mejorar la media-------
-
-#---------------------Promedio de libras gastadas----------------------------
-print('El promedio de libras gastadas por compra es  {} ' .format(df_pivot['Monto'].mean()))
-print('la desviación estandar de libras por compra es  {} ' .format(df_pivot['Monto'].std()))
-#--------------- Mismo que lo anterior----------------
-
-
-#--------------------------Dividir grupo de clientes segun dinero gastado (premium/normal)-------
-df_pivot = df_bd_preprocesada[['CustomerID', 'Monto']].groupby(['CustomerID']).sum()
-df_pivot=df_pivot.reset_index()
-MontoMedio= df_pivot[['Monto']].mean().item()
-#----------------Se define monto medio como criterio para separar clientes ----------------------
-premium= []
-normal = []
-for i in range(0, len(df_pivot)):
-    if df_pivot.iloc[i]['Monto']<= MontoMedio:
-        normal.append(df_pivot.iloc[i]['CustomerID'])
-    else:
-        premium.append(df_pivot.iloc[i]['CustomerID'])
-
-print("Cantidad total de clientes {}" .format(len(df_pivot)))
-print("Cantidad de clientes premium {}" .format(len(premium)))
-print("Cantidad de clientes normales {}" .format(len(normal)))
-
-
-
-
-#------------------------------Intervalo de menor y mayor numero de transacciones-------------------
-df_pivot = df_bd_preprocesada['Hora'].str.split(expand = True, pat =":")
-print("la hora donde más se compra es {} horas" .format(df_pivot[0].mode().item()))
-df_pivot = df_pivot.groupby([0]).count()
-df_pivot=df_pivot.reset_index()
-valorMin = df_pivot[1].min().item() #-- Se define valor minimo de transacciones
-min = 0
-for i in range(0, len(df_pivot)): #--- Se realiza comparación para saber hora de minimás transacciones
-    if valorMin == df_pivot.iloc[i][1]:
-        min = df_pivot.iloc[i][0]
-print("la hora donde menos se compra es {} horas" .format(min))
+#--------------------Llamado a funciones ------------------
+articulos_libras_promedio (df_bd_preprocesada)
+grupo_cliente(df_bd_preprocesada)
+numero_transacciones(df_bd_preprocesada)
